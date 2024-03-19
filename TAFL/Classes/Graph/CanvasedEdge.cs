aaf6c7c4af7d6e6bfb8572af927aba11b71c4125
Никита Фомин <defdXs@yandex.ru>
2024-03-19 23:13:18 +0300

    lol kekw solved

--- TAFL/Classes/Graph/CanvasedEdge.cs
+++ TAFL/Classes/Graph/CanvasedEdge.cs
@@ -30,18 +30,8 @@ public class CanvasedEdge
 
     public Microsoft.UI.Xaml.Shapes.Path UpdatePath()
     {
-        var halfPi = Math.PI / 2;
         var angle = Math.Atan2(Left.Position.Y - Right.Position.Y, Left.Position.X - Right.Position.X);
-        var absAngle = Math.Abs(angle);
-        var _a = absAngle;
-
-        absAngle = NormalizeAngle(absAngle);
-        var b = absAngle / halfPi;
-        absAngle = NormalizeAngle(absAngle + halfPi);
-        var a = absAngle / halfPi;
-        if (a < 0.1) a = 0.1;
-        if (b < 0.1) b = 0.1;
-        LogService.Error($"{a}, {b}");
+        var _a = angle / Math.PI * 180;
 
         var path = new Microsoft.UI.Xaml.Shapes.Path() { Stroke = new SolidColorBrush(Color.FromArgb(
             DefaultPathStroke.A,
@@ -61,17 +51,12 @@ public class CanvasedEdge
                 new Windows.Foundation.Point(Right.Position.X + Right.Radius, Right.Position.Y + Right.Radius) :
                 new Windows.Foundation.Point(Left.Position.X + Left.Radius, Left.Position.Y + Left.Radius),
             SweepDirection = ToRight ? SweepDirection.Clockwise : SweepDirection.Counterclockwise,
-            Size = new Windows.Foundation.Size(a, b),
+            Size = new Windows.Foundation.Size(0.3, 0.1),
             RotationAngle = _a
         });
         pd.Figures.Add(pf);
         path.Data = pd;
         PathObject = path;
         return path;
-    }
-    private double NormalizeAngle(double angle)
-    {
-        if (angle > Math.PI / 2) angle = Math.PI - angle;
-        return angle;
     }
 }
