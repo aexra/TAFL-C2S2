bba8360abf05f54cf8d8ff18d3ac3a1224c08333
Никита Фомин <defdXs@yandex.ru>
2024-03-19 19:07:36 +0300

    Created class CanvacedEdge

---
+++ TAFL/Classes/Graph/CanvasedEdge.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TAFL.Controls;
+
+namespace TAFL.Classes.Graph;
+public class CanvasedEdge
+{
+    public GraphNodeControl Left;
+    public GraphNodeControl Right;
+    public bool ToRight;
+    public string Weight; // a,b,c,...,e
+
+    public CanvasedEdge(GraphNodeControl left, GraphNodeControl right, bool toRight, string weight)
+    {
+        Left = left;
+        Right = right;
+        ToRight = toRight;
+        Weight = weight;
+    }
+}
