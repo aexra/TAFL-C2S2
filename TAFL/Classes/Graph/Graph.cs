b44200cf53f1a264a7aadcc6426f9668087f3177
Никита Фомин <defdXs@yandex.ru>
2024-03-19 12:15:22 +0300

    Some work with graphs

---
+++ TAFL/Classes/Graph/Graph.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TAFL.Interfaces;
+
+namespace TAFL.Classes.Graph;
+public class Graph : IGraph
+{
+    public List<IGraphNode> Nodes { private set; get; }
+}
