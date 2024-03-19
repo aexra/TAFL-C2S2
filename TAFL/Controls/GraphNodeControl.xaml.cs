f0f34032f1cfa4404727a56facf0e7e52d945ab5
Никита Фомин <defdXs@yandex.ru>
2024-03-19 16:13:13 +0300

    Holy selection

--- TAFL/Controls/GraphNodeControl.xaml.cs
+++ TAFL/Controls/GraphNodeControl.xaml.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Numerics;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using Microsoft.UI.Xaml;
@@ -13,6 +15,7 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using TAFL.Services;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
@@ -20,7 +23,7 @@ using Windows.Foundation.Collections;
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace TAFL.Controls;
-public sealed partial class GraphNodeControl : UserControl
+public sealed partial class GraphNodeControl : UserControl, INotifyPropertyChanged
 {
     public Vector2 Position;
     public float Radius = 40;
@@ -30,8 +33,27 @@ public sealed partial class GraphNodeControl : UserControl
     public float InnerRadius = 34;
     public float InnerDiameter => InnerRadius * 2;
 
-    private bool isSelected = true;
+    public bool IsSelected
+    {
+        get => isSelected;
+        set
+        {
+            if (value != isSelected)
+            {
+                isSelected = value;
+                NotifyPropertyChanged("SelectedBrush");
+            }
+        }
+    }
+    private bool isSelected;
     private readonly Color SelectionColor = Color.DarkOrange;
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
     public Brush SelectedBrush
     {
         get
@@ -51,7 +73,18 @@ public sealed partial class GraphNodeControl : UserControl
     public GraphNodeControl(Vector2 position)
     {
         Position = new Vector2(position.X - Radius, position.Y - Radius);
-
+        isSelected = false;
         this.InitializeComponent();
+    }
+
+    public bool ToggleSelection()
+    {
+        IsSelected = !IsSelected;
+        return isSelected;
+    }
+
+    private void Border_PointerPressed(object sender, PointerRoutedEventArgs e)
+    {
+        ToggleSelection();
     }
 }
