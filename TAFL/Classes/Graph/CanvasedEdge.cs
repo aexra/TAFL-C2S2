cb65a00425517975195e77c35961ed60cb7f2947
Никита Фомин <defdXs@yandex.ru>
2024-03-19 22:44:41 +0300

    mmmmmmmmmmm

--- TAFL/Classes/Graph/CanvasedEdge.cs
+++ TAFL/Classes/Graph/CanvasedEdge.cs
@@ -33,6 +33,7 @@ public class CanvasedEdge
         var halfPi = Math.PI / 2;
         var angle = Math.Atan2(Left.Position.Y - Right.Position.Y, Left.Position.X - Right.Position.X);
         var absAngle = Math.Abs(angle);
+        var _a = absAngle;
 
         absAngle = NormalizeAngle(absAngle);
         var b = absAngle / halfPi;
@@ -60,7 +61,8 @@ public class CanvasedEdge
                 new Windows.Foundation.Point(Right.Position.X + Right.Radius, Right.Position.Y + Right.Radius) :
                 new Windows.Foundation.Point(Left.Position.X + Left.Radius, Left.Position.Y + Left.Radius),
             SweepDirection = ToRight ? SweepDirection.Clockwise : SweepDirection.Counterclockwise,
-            Size = new Windows.Foundation.Size(a, b)
+            Size = new Windows.Foundation.Size(a, b),
+            RotationAngle = _a
         });
         pd.Figures.Add(pf);
         path.Data = pd;
