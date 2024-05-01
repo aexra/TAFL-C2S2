36ebffbb87dc0beffd527038e1386afcdccfff57
Никита Фомин <defdXs@yandex.ru>
2024-05-01 12:20:46 +0300

    Deleted ALog submodule

--- TAFL/Views/Lab7Page.xaml.cs
+++ TAFL/Views/Lab7Page.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.UI.Xaml.Controls;
-
+using TAFL.Misc;
 using TAFL.ViewModels;
 
 namespace TAFL.Views;
@@ -15,5 +15,38 @@ public sealed partial class Lab7Page : Page
     {
         ViewModel = App.GetService<Lab7ViewModel>();
         InitializeComponent();
+    }
+
+    private void BasedSymbolsBox_TextChanging(TextBox sender, TextBoxTextChangingEventArgs args)
+    {
+        var distinct = StringUtils.StringToDistinctString(sender.Text);
+        if (distinct != sender.Text)
+        {
+            var pointer = sender.SelectionStart - 1;
+            sender.Text = distinct;
+            sender.SelectionStart = pointer;
+        }
+    }
+
+    private void StartSymbolsBox_TextChanging(TextBox sender, TextBoxTextChangingEventArgs args)
+    {
+        var distinct = StringUtils.StringToDistinctString(sender.Text);
+        if (distinct != sender.Text)
+        {
+            var pointer = sender.SelectionStart - 1;
+            sender.Text = distinct;
+            sender.SelectionStart = pointer;
+        }
+    }
+
+    private void EndSymbolsBox_TextChanging(TextBox sender, TextBoxTextChangingEventArgs args)
+    {
+        var distinct = StringUtils.StringToDistinctString(sender.Text);
+        if (distinct != sender.Text)
+        {
+            var pointer = sender.SelectionStart - 1;
+            sender.Text = distinct;
+            sender.SelectionStart = pointer;
+        }
     }
 }
